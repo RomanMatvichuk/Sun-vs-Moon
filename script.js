e162185e9a5e30455072d747bf6672ab3c9af77b
Roman Matvichuk <maro19xr@student.ju.se>
2019-09-05 21:34:51 +0200

    Update

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ document.getElementById("pic").onclick = function(){
     var pic = document.getElementById("pic").getAttribute("src");
     
     switch (pic){
-
         case "images/moon.jpg":
             document.getElementById("pic").setAttribute("src", "images/sun.jpg")
             break;
         case "images/sun.jpg":
             document.getElementById("pic").setAttribute("src", "images/moon.jpg")
-            break;
-            
+            break;            
     }
 }
